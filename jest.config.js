08db9958fa653e6307aada25acfc1ef6a581a698
Blake Dietz <blakedietz@gmail.com>
2019-01-30 22:03:23 -0700

    feat(trie): Adds trie
    Removes a bunch of basic scaffold code along with adding typescript
    basics. This is a bad commit folks.

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "js",
+    "json",
+    "node"
+  ],
+};
