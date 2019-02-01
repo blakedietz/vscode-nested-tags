95e258b24d50460f7cb706411a0d5538a296b376
Blake Dietz <blakedietz@gmail.com>
2019-02-01 16:51:58 -0700

    chore(test): Add some test helpers to serialize the tree

--- src/trie/tag-tree.test.ts
+++ src/trie/tag-tree.test.ts
@@ -1,25 +1,29 @@
-import { TagTree } from "./trie";
+import { TagTree } from "./tag-tree";
+import { convertTreeToObject } from "./trie-test-helpers";
 
 describe("Trie", () => {
   describe("addNode", () => {
     test("Single depth path", () => {
       const tagTree = new TagTree();
       tagTree.addFile("foo.md", ["hello"], "foo.md");
-      console.log(tagTree);
+      console.log(convertTreeToObject(tagTree));
+      console.log(convertTreeToObject(tagTree).root.tags.hello.files["foo.md"]);
     });
     test("Two separate branches", () => {
       const tagTree = new TagTree();
       tagTree.addFile("foo.md", ["hello", "world"], "foo.md");
-      console.log(tagTree);
+      console.log(convertTreeToObject(tagTree).root.tags.hello.files["foo.md"]);
+      console.log(convertTreeToObject(tagTree).root.tags.world.files["foo.md"]);
     });
     test("Two directories deep", () => {
       const tagTree = new TagTree();
       tagTree.addFile("foo.md", ["hello/world"], "foo.md");
-      console.log(tagTree);
+      console.log(
+        convertTreeToObject(tagTree).root.tags.hello.tags.world.files["foo.md"]
+      );
     });
 
-    test("No depth path", () => {});
-
+    // test("No depth path", () => {});
     // TODO: (bdietz) - delete when the tree looks like this
     // A -> B -> C -> file.blah
     // would need to crawl back up the tree to make sure that
