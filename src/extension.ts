543955444b2942b60e492861936246c9d8acc017
Blake Dietz <blakedietz@gmail.com>
2019-12-04 16:21:50 -0700

    fix(extension.ts): Remove an unnecessary console.log

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { TagTreeView } from "./tag-tree-view";
-console.log("test");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
